6a03848627e40f6c5b041f44b6062ddebc2d0792
Victor-YG <yizhougao8@gmail.com>
2024-06-12 22:49:24 -0400

    Compute angular acceleration based on the function of omega.

--- sim/gen_trajectory.m
+++ sim/gen_trajectory.m
@@ -85,6 +85,12 @@ for k = 1 : N
     omega(3) = w_z_0 + w_z_1 * sin(w_z_2 * t + w_z_3) + w_z_4 * cos(w_z_5 * t + w_z_6);
     omega_all(k, :) = omega;
 
+    % derived function for angular acceleration (vehicle frame)
+    alpha(1) = w_x_1 * w_x_2 * cos(w_x_2 * t + w_x_3) - w_x_4 * w_x_5 * sin(w_x_5 * t + w_x_6);
+    alpha(2) = w_y_1 * w_y_2 * cos(w_y_2 * t + w_y_3) - w_y_4 * w_y_5 * sin(w_y_5 * t + w_y_6);
+    alpha(3) = w_z_1 * w_z_2 * cos(w_z_2 * t + w_z_3) - w_z_4 * w_z_5 * sin(w_z_5 * t + w_z_6);
+    alpha_all(k, :) = alpha;
+
     % artificial function for acceleration (body frame)
     accel(1) = a_x_0 + a_x_1 * sin(a_x_2 * t + a_x_3) + a_x_4 * cos(a_x_5 * t + a_x_6);
     accel(2) = a_y_0 + a_y_1 * sin(a_y_2 * t + a_y_3) + a_y_4 * cos(a_y_5 * t + a_y_6);
